8302d36aeb9ab23e413f5a0081a5fcb686d8ba35
camilozv21 <camilozv21@gmail.com>
2023-10-05 14:05:11 -0500

    Preparing for deploying

---
+++ server/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const connectDB = require('./database/config/db');
+const colors = require('colors')
+const cors = require('cors')
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// Connect to database
+connectDB();
+
+app.use(cors());
+
+// app.get('/', (req, res) => {
+//     res.send(`Bienvenidos a mi servidor`)
+// });
+
+app.listen(port, console.log(`server running on port ${port}`));
