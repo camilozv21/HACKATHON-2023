de9b1d82a40578b9f42f5ffa111acce3ea77df8b
camilozv21 <camilozv21@gmail.com>
2023-10-07 11:39:50 -0500

    Preparing to make features

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import CarouselFade from './components/Carousel';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App background-container">
       <CarouselFade />
     </div>
   );
