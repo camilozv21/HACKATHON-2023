5d4122a2c006c3fc1cc659c2f2b0a25e08aaca22
camilozv21 <camilozv21@gmail.com>
2023-10-07 10:29:39 -0500

    Add caption img

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -2,7 +2,7 @@ import Carousel from 'react-bootstrap/Carousel';
 
 function DarkVariantExample() {
   return (
-    <Carousel data-bs-theme="dark">
+    <Carousel data-bs-theme="dark" fade slide={true}>
       <Carousel.Item>
         <img
           className="d-block w-100 caurosel-img"
@@ -10,8 +10,8 @@ function DarkVariantExample() {
           alt="First slide"
         />
         <Carousel.Caption>
-          <h5 className="t-primary">First slide label</h5>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <h5 className="t-primary">New York</h5>
+          <p className="t-primary">This city had lost 13% of landing area</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -21,8 +21,8 @@ function DarkVariantExample() {
           alt="Second slide"
         />
         <Carousel.Caption>
-          <h5 className="t-primary">Second slide label</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <h5 className="t-primary">Rio de Janeiro</h5>
+          <p className="t-primary">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -32,8 +32,8 @@ function DarkVariantExample() {
           alt="Third slide"
         />
         <Carousel.Caption>
-          <h5 className="t-primary">Third slide label</h5>
-          <p>
+          <h5 className="t-primary">Tokyo</h5>
+          <p className="t-primary">
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
           </p>
         </Carousel.Caption>
