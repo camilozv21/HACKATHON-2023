b516546bef87422b4b8bff46fafa50eec414352b
camilozv21 <camilozv21@gmail.com>
2023-10-07 09:03:25 -0500

    Making changes:

--- server/index.js
+++ server/index.js
@@ -10,11 +10,12 @@ const app = express();
 // Connect to database
 connectDB();
 
-const corsOptions = {
-    origin: 'https://hackathon-2023-udov.vercel.app/', // Reemplaza con el origen de tu cliente
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // Habilita las cookies y cabeceras de autorización (si las usas)
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//     origin: 'https://hackathon-2023-udov.vercel.app/', // Reemplaza con el origen de tu cliente
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     credentials: true, // Habilita las cookies y cabeceras de autorización (si las usas)
+// };
+// app.use(cors(corsOptions));
+app.use(cors());
 
 app.listen(port, console.log(`server running on port ${port}`));
