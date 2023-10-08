375e6fa66a625481c7889f7b4af99b96ac639c98
AstroDanscience <140918872+AstroDanscience@users.noreply.github.com>
2023-10-07 21:53:50 -0500

    xd

--- server/features/analize_file.py
+++ server/features/analize_file.py
@@ -6,20 +6,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import itertools
 from matplotlib.colors import LinearSegmentedColormap
-import sys
 from scipy.stats import pearsonr
 from scipy.stats import spearmanr
 from scipy.stats import kendalltau
 
-def Netcdf2list():
-    
-# Lee la entrada desde stdin
 
-# Hacer algo con la entrada
-    fnp = sys.stdin.read()
-    fn = fnp.rstrip(' \n')
-    print("Input recibido:", fn)
-    # fn=input("Ingrese ruta")
+def Netcdf2list():
+
+    fn=input("Ingrese ruta")
     corr_array=[]
     titles=[]
 
@@ -58,14 +52,63 @@ def Netcdf2list():
 
     # Guarda la lista en el archivo de texto
     np.savetxt(nombre_archivo, final_data, fmt="%s", delimiter=" ")
-
     #print(f"La lista se ha guardado en '{nombre_archivo}'.")
-    return dataframe
 
-print('-------------------------------')
 Netcdf2list()
 
+def plot(df):
+  allPosible = list(itertools.combinations(np.arange(df.shape[1]),2))
+  for pair in allPosible[0:10]:
+    plt.figure()
+    plt.title(f"{df.columns[pair[0]]} vs {df.columns[pair[1]]}")
+    plt.scatter(df[df.columns[pair[0]]],df[df.columns[pair[1]]])
+    plt.xlabel(f'{df.columns[pair[0]]}')
+    plt.ylabel(f'{df.columns[pair[1]]}')
+    plt.show
 
+#plot(Netcdf2list())
+
+route='lista.txt'
+graphics=0
+
+#Titles reading
+def autograph():
+    titles=[]
+    with open(route, 'r') as archivo:
+        titles = archivo.readline().strip().split()
+        print(titles)
+
+    #Obtain all possible combinations
+    data = np.loadtxt(route, skiprows=1)
+    cols=data.shape[1]
+    rand_i=np.arange(cols)
+    print(rand_i)
+    combinations=list(itertools.combinations(rand_i,2))
+    print(combinations)
+    col_secindex=len(combinations)
+
+    for combination in combinations:
+        row=combination[0]
+        col=combination[1]
+        global down_menu
+        down_menu=[]
+        x,y=np.loadtxt(route,skiprows=1,usecols=[row,col],unpack=True)
+        plt.title(f"{titles[row]} vs {titles[col]} ($0 \leq E \leq 1x10^{19}$ eV)",fontname='Times New Roman')
+        down_menu=[titles[row],titles[col]]
+        print(down_menu)
+        plt.xlabel(f'{titles[row]}',fontname='Times New Roman')
+        plt.ylabel(f'{titles[col]}',fontname='Times New Roman')
+        #Seleccion de tipo de grafico 
+        if(graphics==0):
+            plt.scatter(x,y)     
+        elif(graphics==1):
+            cmap = LinearSegmentedColormap.from_list('custom_cmap', [(0, 'purple'), (1, 'red')])
+            plt.hist2d(x,y, bins=100, cmap=cmap)
+        file_name=f"{titles[col]} vs {titles[row]}.jpg"
+        ruta='HACKATHON-2023/server/database/data\Plot'
+
+        plt.savefig(ruta+file_name, bbox_inches='tight') 
+        plt.close()
 
 # Function to generate scatter plots for all column combinations
 def plot(df):
@@ -78,72 +121,19 @@ def plot(df):
     corr_pearson, _ = pearsonr(df[df.columns[pair[0]]], df[df.columns[pair[1]]])
     corr_spearman, _ = spearmanr(df[df.columns[pair[0]]], df[df.columns[pair[1]]])
     corr_kendall, _ = kendalltau(df[df.columns[pair[0]]], df[df.columns[pair[1]]])
-    plt.figure()
-    plt.title(f"{df.columns[pair[0]]} vs {df.columns[pair[1]]}" +
+    plt.figure(figsize=(8,10))
+    file_name=f"{df.columns[pair[0]]} vs {df.columns[pair[1]]}"
+    plt.title( file_name+
               "\n\n Correlacion de Pearson: " + str(corr_pearson) +
               "\n Correlacion de Spearman: " + str(corr_spearman) +
               "\n Correlacion de Kendall: " + str(corr_kendall))
     plt.scatter(df[df.columns[pair[0]]], df[df.columns[pair[1]]])
     plt.xlabel(f'{df.columns[pair[0]]}')
     plt.ylabel(f'{df.columns[pair[1]]}')
-    plt.savefig(f"{df.columns[pair[0]]} vs {df.columns[pair[1]]}")
+    ruta_corr='server/database/data/image/correlation'
+    plt.savefig(ruta_corr+file_name+'.jpg')
     plt.close()
-    # plt.show()
-
-angie = pd.read_csv("lista.txt", sep=" ", index_col=0)
-
-# plot(angie)
-
-
-# def plot(df):
-#   allPosible = list(itertools.combinations(np.arange(df.shape[1]),2))
-#   for pair in allPosible[0:10]:
-#     plt.figure()
-#     plt.title(f"{df.columns[pair[0]]} vs {df.columns[pair[1]]}")
-#     plt.scatter(df[df.columns[pair[0]]],df[df.columns[pair[1]]])
-#     plt.xlabel(f'{df.columns[pair[0]]}')
-#     plt.ylabel(f'{df.columns[pair[1]]}')
-#     plt.show
-
-# # plot(Netcdf2list())
-
-
-# route='list.txt'
-# graphics=0
-
-# #Titles reading
-# def autograph():
-#     titles=[]
-#     with open(route, 'r') as archivo:
-#         titles = archivo.readline().strip().split()
-#         print(titles)
-
-#     #Obtain all possible combinations
-#     data = np.loadtxt(route, skiprows=1)
-#     cols=data.shape[1]
-#     rand_i=np.arange(cols)
-#     print(rand_i)
-#     combinations=list(itertools.combinations(rand_i,2))
-#     print(combinations)
-#     col_secindex=len(combinations)
-
-#     for combination in combinations:
-#         row=combination[0]
-#         col=combination[1]
-#         global down_menu
-#         down_menu=[]
-#         x,y=np.loadtxt(route,skiprows=1,usecols=[row,col],unpack=True)
-#         plt.title(f"{titles[row]} vs {titles[col]} ($0 \leq E \leq 1x10^{19}$ eV)",fontname='Times New Roman')
-#         down_menu=[titles[row],titles[col]]
-#         print(down_menu)
-#         plt.xlabel(f'{titles[row]}',fontname='Times New Roman')
-#         plt.ylabel(f'{titles[col]}',fontname='Times New Roman')
-#         #Seleccion de tipo de grafico 
-#         if(graphics==0):
-#             plt.scatter(x,y)     
-#         elif(graphics==1):
-#             cmap = LinearSegmentedColormap.from_list('custom_cmap', [(0, 'purple'), (1, 'red')])
-#             plt.hist2d(x,y, bins=100, cmap=cmap)
-
-#         plt.savefig(f"{titles[col]} vs {titles[row]}.jpg", bbox_inches='tight') 
-#         plt.close()
+    #plt.show()
+  
+data = pd.read_csv("lista.txt", sep=" ", index_col=0)
+plot(data)
